1709101ffc3ea2e9068edf721f3af2ea170adf8e
Giovanna Ribeiro <gribeiro@microsoft.com>
2024-09-17 20:23:51 -0700

    adds simple one note extension

---
+++ dndOneNote/src/taskpane/taskpane.js
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+
+/* global document, Office */
+
+/*const { AzureOpenAI } = require("openai");
+
+// You will need to set these environment variables or edit the following values
+const endpoint = process.env["AZURE_OPENAI_ENDPOINT"] || "<endpoint>";
+const apiKey = process.env["AZURE_OPENAI_API_KEY"] || "<api key>";
+const apiVersion = "2024-05-01-preview";
+const deployment = "gpt-4o"; //This must match your deployment name.
+require("dotenv/config");*/
+
+Office.onReady((info) => {
+  if (info.host === Office.HostType.OneNote) {
+    document.getElementById("sideload-msg").style.display = "none";
+    document.getElementById("app-body").style.display = "flex";
+    document.getElementById("run").onclick = run;
+  }
+});
+
+export async function run() {
+  try {
+    await OneNote.run(async (context) => {
+
+        // Get the current page.
+        const page = context.application.getActivePage();
+
+        // Queue a command to set the page title.
+        page.title = "Hello World";
+
+        // Queue a command to add an outline to the page.
+        const html = "<p><ol><li>Item #1</li><li>Item #2</li></ol></p>";
+        page.addOutline(40, 90, html);
+
+        // Run the queued commands.
+        await context.sync();
+    });
+  } catch (error) {
+      console.log("Error: " + error);
+  }
+}
